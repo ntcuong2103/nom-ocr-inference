7a35ae7621778f4c9f045d0b6cf4b38e89fe84c8
Cuong Tuan Nguyen <ntcuong2103@gmail.com>
2025-11-02 16:39:52 +0000

    add multi bbox load

--- data.py
+++ data.py
@@ -7,6 +7,90 @@ from PIL import Image
 from pathlib import Path
 import imagesize
 
+class ImageDataset(Dataset):
+    def __init__(self, image_dir, label_dir, vocab, transform=None, expand_ratio=1.2):
+        self.image_dir = Path(image_dir) # Convert to Path object
+        self.label_dir = Path(label_dir) # Convert to Path object
+        self.transform = transform
+        self.vocab = vocab
+        self.image_paths = []
+        self.label_paths = []
+        self.expand_ratio = expand_ratio
+        self.load_data()
+
+    def load_data(self):
+        # Clear previous lists in case load_data is called multiple times
+        self.image_paths = []
+        self.label_paths = []
+
+        for image_path in self.image_dir.rglob('*'): # Use rglob for recursive search
+            if image_path.suffix.lower() in ('.png', '.jpg', '.jpeg'):
+                # Construct corresponding label path
+                relative_path = image_path.relative_to(self.image_dir)
+                label_path = (self.label_dir / relative_path).with_suffix('.txt')
+
+                # Check if label file exists
+                if label_path.exists():
+                    self.image_paths.append(image_path)
+                    self.label_paths.append(label_path)
+                else:
+                    print(f"Label file not found for {image_path}. Skipping.")
+
+    def __len__(self):
+        return len(self.image_paths)
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        label_path = self.label_paths[idx]
+        image = Image.open(image_path).convert('RGB')
+
+        # read label YOLO format
+        with open(label_path, 'r') as f:
+            labels = f.readlines()
+        classes = [label.strip().split()[0] for label in labels]
+        bboxes = [list(map(float, label.strip().split()[1:])) for label in labels]
+        classes = np.array(classes)
+        bboxes = np.array(bboxes)
+        bboxes = bboxes * np.array([image.width, image.height, image.width, image.height])
+        bboxes = bboxes.astype(int)
+
+    
+        images = []
+        out_classes = []
+        indices = []
+        for box_id, (bbox,cls) in enumerate(zip(bboxes, classes)):
+            if cls not in self.vocab.ids_dict and cls != '0':
+                continue
+            x, y, w, h = bbox
+            # make equal crop
+            w = max(w, h) * self.expand_ratio
+            h = w
+
+            # calculate coordinates
+            x1 = max(0, int(x - w / 2))
+            y1 = max(0, int(y - h / 2))
+            x2 = min(image.width, int(x + w / 2))
+            y2 = min(image.height, int(y + h / 2))
+            # crop the image
+            image_cropped = image.crop((x1, y1, x2, y2))
+            # check size of the cropped image
+            if image_cropped.size[0] < 1 or image_cropped.size[1] < 1:
+                print(f"Invalid crop size for {image_path} at index {box_id}. Skipping.")
+                continue
+
+            if self.transform:
+                image_cropped = self.transform(image_cropped)
+            
+            # # save cropped image for visualization
+            # os.makedirs(os.path.join(self.data_dir, 'crops'), exist_ok=True)
+            # image_cropped.save(os.path.join(self.data_dir, 'crops', f'{idx}_{cls}.png'))
+            
+            images.append(image_cropped)
+            out_classes.append(cls)
+            indices.append(box_id)
+        
+        return [f'{path}@{idx}' for path, idx in zip([image_path] * len(indices), indices) ], images, [self.vocab.encode(cls) for cls in out_classes]
+
+
 class ImageDatasetBBox(Dataset):
     def __init__(self, image_dir, label_dir, vocab, transform=None, expand_ratio=1.2):
         self.transform = transform
@@ -91,7 +175,7 @@ if __name__ == "__main__":
             transforms.RandomInvert(p=1.0),
         ])
 
-    dataset = ImageDatasetBBox(
+    dataset = ImageDataset(
         image_dir='datasets/nomnaocr/images',
         label_dir='detection/nomnaocr/labels',
         vocab=SeqVocab(base_vocab, ids_dict),
